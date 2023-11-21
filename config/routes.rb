ee7c792061ad90a504a33267f359f79b131d9223
gupt-mudit <muditgupt01@gmail.com>
2023-11-21 18:13:06 +0530

    A useful message to help remember details of commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :categories
+  
+  resources :articles
+  root 'home#index'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+end
